f0dd52f19f6a1f506acb43008b73f4427e55e243
davekj1 <76756514+davekj1@users.noreply.github.com>
2022-06-11 14:15:06 +0530

    Add files via upload

---
+++ day7/Solution3.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null){
+            return list2;
+        }
+        if(list2==null)
+            return list1;
+        if(list1==null && list2==null)
+            return null;
+        ListNode m;
+        if(list1.val>list2.val){
+            m=list2;
+            list2=list2.next;
+        }
+        else{
+            m=list1;
+            list1=list1.next;
+        }
+        m.next=mergeTwoLists(list1,list2);
+        return m;
+    }
+}
